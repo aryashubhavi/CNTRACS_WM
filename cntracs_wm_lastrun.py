afb7c064f09983bf2ae41eb6af1eee9e24461866
Shubhavi Arya <aryax014@umn.edu>
2020-10-25 04:00:31 -0500

    update

--- cntracs_wm_lastrun.py
+++ cntracs_wm_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v3.2.4),
-    on Wed Aug 12 15:41:38 2020
+    on Sun Oct  4 15:49:36 2020
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -49,7 +49,7 @@ filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expNa
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
-    originPath='/Users/shubhaviarya/Downloads/WM_Encoding/cntracs_wm_lastrun.py',
+    originPath='/Users/shubhaviarya/Desktop/Courses/UMN/tricam/CNTRACS_WM/cntracs_wm_lastrun.py',
     savePickle=True, saveWideText=True,
     dataFileName=filename)
 # save a log file for detail verbose info
